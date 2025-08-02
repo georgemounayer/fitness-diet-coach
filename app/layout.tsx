df9acef4fcc8384d4ede0552ff465d8a28eec06b
georgemounayer <styrsobar@gmail.com>
2025-08-02 20:35:20 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="sv">
-      <head /> {/* 🟢 MÅSTE finnas */}
+      <head /> {/* 🔧 Måste finnas med */}
       <body>{children}</body>
     </html>
   )
