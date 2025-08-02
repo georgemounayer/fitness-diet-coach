b4d71f2ee0b628dd67e01d75030e4fff32f4ba92
georgemounayer <styrsobar@gmail.com>
2025-08-02 22:29:29 +0200

    Delete app directory

--- app/auth/signup.tsx
+++
@@ -1,8 +0,0 @@
-export default function Signup() {
-  return (
-    <main>
-      <h1>Skapa konto</h1>
-      <p>Här kommer registreringsformuläret.</p>
-    </main>
-  );
-}
--- app/onboarding/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function OnboardingPage() {
-  return (
-    <main>
-      <h1>Välkommen till Fitness & Diet Coach!</h1>
-      <p>Din personliga resa börjar här.</p>
-    </main>
-  );
-}
