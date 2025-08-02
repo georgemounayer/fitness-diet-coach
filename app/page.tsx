edd1552cf2ada16cf9af98b0650c2980370516a5
georgemounayer <styrsobar@gmail.com>
2025-08-02 21:10:19 +0200

    Add files via upload
    v1

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h1>Välkommen till Fitness Diet Coach</h1>;
+}
---
+++ onboarding/page.tsx
@@ -0,0 +1,8 @@
+export default function Onboarding() {
+  return (
+    <div>
+      <h1>Onboarding</h1>
+      <p>Här kommer onboarding-flödet att visas.</p>
+    </div>
+  );
+}
