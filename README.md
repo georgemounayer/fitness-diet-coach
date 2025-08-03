0f86b2498cb139d014221bf2a758b9e32f88ef95
georgemounayer <styrsobar@gmail.com>
2025-08-03 14:08:41 +0200

    Raderade lokala filer manuellt i Finder

--- README.md
+++
@@ -1,3 +0,0 @@
-# Fitness Diet Coach v1.4.4
-
-Stable version with clean app structure.
--- components/DailySummary.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./DailySummary.module.css";
-
-export default function DailySummary() {
-  return (
-    <div className={styles.summary}>
-      <h1>Dagens sammanfattning</h1>
-    </div>
-  );
-}
